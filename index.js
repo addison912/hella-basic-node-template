7906de4dd1592194e8da7a60a829ce7ae28b162f
Addison Moore <addisondrewmoore@gmail.com>
2021-03-26 00:23:03 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express"),
+  path = require("path"),
+  app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const port = process.env.PORT || 3000;
+let build = process.env.NODE_ENV == "development" ? "../dist" : "build";
+console.log(
+  `${process.env.NODE_ENV == "development" ? "Dev" : "Production"} build`
+);
+
+app.use("/", express.static(path.join(__dirname, build)));
+
+//run server on port 3000
+app.listen(port, () => {
+  console.log(`app listening at port ${port}`);
+});
