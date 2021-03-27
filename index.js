18f0a3933f41c0bc94e486059b8f606b23c4777b
Addison Moore <addisondrewmoore@gmail.com>
2021-03-26 21:27:49 -0700

    added .env

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require("express"),
   path = require("path"),
   app = express();
 
+require("dotenv").config();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const port = process.env.PORT || 3000;
-let build = process.env.NODE_ENV == "development" ? "../dist" : "build";
+let build = process.env.NODE_ENV == "development" ? "dist" : "build";
 console.log(
   `${process.env.NODE_ENV == "development" ? "Dev" : "Production"} build`
 );
